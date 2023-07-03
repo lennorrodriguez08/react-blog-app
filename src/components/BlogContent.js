40d048e6fceb3e4ae7295c4b727423a56b4cdeb8
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-03 23:49:16 +0800

    Install font awesome

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -2,13 +2,12 @@ import classes from './BlogContent.module.css'
 
 function BlogContent( { blogsData } ) {
 
-    console.log(typeof blogsData[0].id)
     return (
         <div id="blog-content">
             { blogsData.map(blog => {
                 return (
                     <article className={classes.article} id={blog.id} key={blog.id}>
-                        <img src={blog.image_url} alt="" width='100%' />
+                        <img src={blog.image_url} alt="Blog image" width='100%' />
                         <h2>{blog.title}</h2>
                         <p>{blog.author}</p>
                         <p>{blog.date_posted}</p>
