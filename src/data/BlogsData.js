fbc6bd11c26bc44ec86f9c9de82bba1f2a745bc9
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-03 17:26:20 +0800

    Do rename and export data file

---
+++ src/data/BlogsData.js
@@ -0,0 +1,31 @@
+const BlogsData = [
+    {
+        id: 1,
+        image_url: 'https://placehold.co/1200x630',
+        title: 'Let\'s get in touch in LinkedIn',
+        introductory: 'Nulla laoreet lorem pulvinar maximus sodales. Donec non sapien purus. Praesent pulvinar tellus eget sagittis suscipit. Aenean mi odio, semper sit amet ipsum accumsan, feugiat interdum ex. Etiam ac enim interdum, elementum mauris eu, tincidunt ligula. Phasellus condimentum feugiat felis condimentum tempus. Pellentesque malesuada tristique bibendum. Vestibulum nec sapien maximus.',
+        article: 'Nulla facilisi. Aenean condimentum est sagittis orci viverra, in volutpat nibh pulvinar. Suspendisse massa elit, tincidunt et consequat vitae, vestibulum nec odio. Aenean feugiat ligula massa, pulvinar blandit nulla semper eu. Integer non tortor risus. Suspendisse fringilla efficitur sodales. Suspendisse potenti. Sed et quam eget velit condimentum volutpat. Ut quam sapien, ullamcorper non ligula at, tristique pharetra quam. Nunc tristique, neque in varius pretium, mi felis dignissim tortor, sed luctus ipsum lectus ac dui. Fusce ultrices convallis mi, sit amet aliquet velit ornare eget. Praesent sed ornare dui. Nullam ornare, diam et fringilla molestie, nibh lacus egestas purus, eget faucibus libero nunc non nisi. Aenean massa elit, egestas vitae bibendum non, maximus nec eros. Donec eleifend, lectus non aliquet vestibulum, augue nulla mattis mauris, a mattis lacus nibh at libero.',
+        author: 'Ronnel Rodriguez',
+        date_posted: 'July 03, 2023'
+    },
+    {
+        id: 2,
+        image_url: 'https://placehold.co/1200x630',
+        title: 'Visit my GitHub repositories',
+        introductory: 'Nulla laoreet lorem pulvinar maximus sodales. Donec non sapien purus. Praesent pulvinar tellus eget sagittis suscipit. Aenean mi odio, semper sit amet ipsum accumsan, feugiat interdum ex. Etiam ac enim interdum, elementum mauris eu, tincidunt ligula. Phasellus condimentum feugiat felis condimentum tempus. Pellentesque malesuada tristique bibendum. Vestibulum nec sapien maximus.',
+        article: 'Nulla facilisi. Aenean condimentum est sagittis orci viverra, in volutpat nibh pulvinar. Suspendisse massa elit, tincidunt et consequat vitae, vestibulum nec odio. Aenean feugiat ligula massa, pulvinar blandit nulla semper eu. Integer non tortor risus. Suspendisse fringilla efficitur sodales. Suspendisse potenti. Sed et quam eget velit condimentum volutpat. Ut quam sapien, ullamcorper non ligula at, tristique pharetra quam. Nunc tristique, neque in varius pretium, mi felis dignissim tortor, sed luctus ipsum lectus ac dui. Fusce ultrices convallis mi, sit amet aliquet velit ornare eget. Praesent sed ornare dui. Nullam ornare, diam et fringilla molestie, nibh lacus egestas purus, eget faucibus libero nunc non nisi. Aenean massa elit, egestas vitae bibendum non, maximus nec eros. Donec eleifend, lectus non aliquet vestibulum, augue nulla mattis mauris, a mattis lacus nibh at libero.',
+        author: 'Ronnel Rodriguez',
+        date_posted: 'July 03, 2023'
+    },
+    {
+        id: 3,
+        image_url: 'https://placehold.co/1200x630',
+        title: 'Watch my project recording in YouTube',
+        introductory: 'Nulla laoreet lorem pulvinar maximus sodales. Donec non sapien purus. Praesent pulvinar tellus eget sagittis suscipit. Aenean mi odio, semper sit amet ipsum accumsan, feugiat interdum ex. Etiam ac enim interdum, elementum mauris eu, tincidunt ligula. Phasellus condimentum feugiat felis condimentum tempus. Pellentesque malesuada tristique bibendum. Vestibulum nec sapien maximus.',
+        article: 'Nulla facilisi. Aenean condimentum est sagittis orci viverra, in volutpat nibh pulvinar. Suspendisse massa elit, tincidunt et consequat vitae, vestibulum nec odio. Aenean feugiat ligula massa, pulvinar blandit nulla semper eu. Integer non tortor risus. Suspendisse fringilla efficitur sodales. Suspendisse potenti. Sed et quam eget velit condimentum volutpat. Ut quam sapien, ullamcorper non ligula at, tristique pharetra quam. Nunc tristique, neque in varius pretium, mi felis dignissim tortor, sed luctus ipsum lectus ac dui. Fusce ultrices convallis mi, sit amet aliquet velit ornare eget. Praesent sed ornare dui. Nullam ornare, diam et fringilla molestie, nibh lacus egestas purus, eget faucibus libero nunc non nisi. Aenean massa elit, egestas vitae bibendum non, maximus nec eros. Donec eleifend, lectus non aliquet vestibulum, augue nulla mattis mauris, a mattis lacus nibh at libero.',
+        author: 'Ronnel Rodriguez',
+        date_posted: 'July 03, 2023'
+    },
+]
+
+export default BlogsData
