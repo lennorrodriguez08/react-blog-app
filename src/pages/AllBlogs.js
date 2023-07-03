2798bbaa061d91462b72368ee5490d7ad5df19cb
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-03 17:25:39 +0800

    Add AllBlogsContent component and pass the BlogsData props

--- src/pages/AllBlogs.js
+++ src/pages/AllBlogs.js
@@ -1,8 +1,11 @@
-function AllBlogs() {
+import AllBlogsContent from '../components/AllBlogsContent'
 
+function AllBlogs( { blogsData } ) {
+   
     return (
         <div>
             <h1>All Blogs Page</h1>
+            <AllBlogsContent blogsData={blogsData} />
         </div>
     )
 
