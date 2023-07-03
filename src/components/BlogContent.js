2a33b877911c91c538c19433bf76ad7931de49a9
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-03 23:50:53 +0800

    Fix blog content spacing. Add icons and button component

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,4 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPen, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import Button from '../shared/Button'
 import classes from './BlogContent.module.css'
+
 
 function BlogContent( { blogsData } ) {
 
@@ -7,11 +11,12 @@ function BlogContent( { blogsData } ) {
             { blogsData.map(blog => {
                 return (
                     <article className={classes.article} id={blog.id} key={blog.id}>
-                        <img src={blog.image_url} alt="Blog image" width='100%' />
+                        <img src={blog.image_url} alt="Blog" width='100%' />
                         <h2>{blog.title}</h2>
-                        <p>{blog.author}</p>
-                        <p>{blog.date_posted}</p>
+                        <p><FontAwesomeIcon icon={faUserPen} /> {blog.author}</p>
+                        <p><FontAwesomeIcon icon={faCalendar} />&nbsp;&nbsp;{blog.date_posted}</p>
                         <p>{blog.introductory}</p>
+                        <Button>Read more</Button>
                     </article>
                 )
             }) }
