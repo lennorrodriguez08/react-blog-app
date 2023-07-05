dd8fc243421ac2911072f46fdcf10b344616919a
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-06 02:56:11 +0800

    Add update functionality. Add and modify helper class.

--- src/pages/ModifyBlog.js
+++ src/pages/ModifyBlog.js
@@ -1,11 +1,11 @@
 import ModifyBlogForm from "../components/ModifyBlogForm"
 
-function ModifyBlog( { blogsData, addBlog } ) {
+function ModifyBlog( { blogsData, modifyBlogs } ) {
 
     return (
         <div>
             <h1>Modify Blog Page</h1>
-            <ModifyBlogForm blogsData={blogsData} addBlog={addBlog} />
+            <ModifyBlogForm blogsData={blogsData} modifyBlogs={modifyBlogs} />
         </div>
     )
 
