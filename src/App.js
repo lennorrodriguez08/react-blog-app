b56bfc0bdefb0656049b9a2eb400a37bc4b4b8bd
Ronnel Rodriguez <lennorrodriguez08@yahoo.com>
2023-07-07 03:45:24 +0800

    Wrap all components to Context Provided. Remove props to avoid prop drilling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Navigation from "./components/Navigation";
 import AllBlogs from "./pages/AllBlogs";
 import ModifyBlog from "./pages/ModifyBlog";
 import Container from "./layout/Container";
-import BlogsData from "./data/BlogsData";
 import BlogArticle from "./components/BlogArticle";
+import { BlogProvider } from "./context/BlogContext";
 
 function App() {
-  const [blogsData, setBlogsData] = useState(BlogsData)
-
-  const modifyBlogs = (newData) => {
-    setBlogsData(newData)
-  }
   
   return (
-    <Router>
-      <Navigation />
+    <BlogProvider>
+      <Router>
+        <Navigation />
         <Container>
           <Routes>
-            <Route exact path='/' element={ <AllBlogs blogsData={blogsData} /> } />
-            <Route path='/modify-blog' element={ <ModifyBlog blogsData={blogsData} modifyBlogs={modifyBlogs} /> } />
-            <Route path="blogs/:id" element={ <BlogArticle blogsData={blogsData} /> } exact />
+            <Route exact path='/' element={ <AllBlogs /> } />
+            <Route path='/modify-blog' element={ <ModifyBlog /> } />
+            <Route path="blogs/:id" element={ <BlogArticle /> } />
           </Routes>
         </Container>
-    </Router>
+      </Router>
+    </BlogProvider>
   );
 }
 
